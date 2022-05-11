7dbb5a09efd261ef1f71ed11df79d4252b999d6e
aleksandra-ovuka <aleksandraovuka95@gmail.com>
2022-05-11 16:02:20 +0200

    commit

---
+++ hw13.php
@@ -0,0 +1,46 @@
+<?php
+//Funkcija za povrsinu kruznog bazena
+function PovrsinaKruga ($a) {
+$sqr=pow(2*$a,2);
+$pk= pi()*$sqr/4;
+return $pk;
+}
+echo PovrsinaKruga(5). " je povrsina ovog kruga <br>";
+
+
+//Funkcija za povrsinu pravougaonika
+
+function PovrsinaPravougaonika ($b,$c) {
+   $pp=$b*$c;
+    return $pp;
+
+}
+echo PovrsinaPravougaonika(6,8). " je povrsina ovog pravougaonika <br>";
+
+// Funkcija za povrsinu bazena sa polukrugom
+
+function PovrsinaBazena ($a,$b,) {
+$pb= PovrsinaPravougaonika($a,$b) + PovrsinaKruga($a)/2;
+return $pb;
+  
+}
+
+echo PovrsinaBazena(4,7). " je povrsina ovog bazena <br>";
+
+// Funkcija za povrsinu svih bazena 
+function PovrsinaSvihBazena ($a1,$b1,$a2,$b2,$r1) {
+    $psb= PovrsinaPravougaonika($a1,$b1) + PovrsinaKruga($r1) + PovrsinaBazena($a2,$b2) ;
+        return $psb;
+    
+}
+echo PovrsinaSvihBazena (6,8,4,7,5). " je povrsina svih bazena zajedno";
+
+
+
+
+
+
+
+
+
+?>
