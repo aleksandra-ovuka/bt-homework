42f9a572f851f01671b88066d92218e30166f5f2
aleksandra-ovuka <aleksandraovuka95@gmail.com>
2022-05-14 20:54:11 +0200

    commit final change

--- hw14/zadatak2.php
+++ hw14/zadatak2.php
@@ -1,15 +1,14 @@
 <?php
 
 function FunkcijaZaKrofne ( $UkupnoBrasno, $BrasnoPoKrofni,$OstatakFlag=NULL) {
-    $BrojKrofni=0;
-    for ($UkupnoBrasno;$UkupnoBrasno>=$BrasnoPoKrofni;$UkupnoBrasno-=$BrasnoPoKrofni,$BrojKrofni++) {}
+    for ($BrojKrofni=0;$UkupnoBrasno>=$BrasnoPoKrofni;$UkupnoBrasno-=$BrasnoPoKrofni,$BrojKrofni++) {}
     if (is_null($OstatakFlag)) $OstatakFlag==false;
     if ($OstatakFlag==false)  return $UkupnoBrasno; else return $BrojKrofni;
   
 }
 
-echo "Napravljeno je ukupno ". FunkcijaZaKrofne(456,10,true)." krofni. <br>"; 
-echo "Preostalo je ". FunkcijaZaKrofne(456,10)." grama brasna. <br>"; 
+echo "Napravljeno je ukupno ". FunkcijaZaKrofne(455,10,true)." krofni. <br>"; 
+echo "Preostalo je ". FunkcijaZaKrofne(455,10)." grama brasna. <br>"; 
 
 
 ?>
